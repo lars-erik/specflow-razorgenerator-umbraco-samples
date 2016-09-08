e34820d91797dfd16f4a5fb73d78709e474cde89
Lars-Erik Aabech <lars-erik.aabech@markedspartner.no>
2016-09-08 21:08:14 +0200

    Infrastructure for Umbraco done. Need to pull in regular MVC samples.

--- Sample.Tests/MinimalReadContentTypeWithGrid.cs
+++ Sample.Tests/MinimalReadContentTypeWithGrid.cs
@@ -1,12 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web.Mvc;
 using System.Web.Routing;
 using ASP;
 using Moq;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using RazorGenerator.Testing;
+using TechTalk.SpecFlow;
 using Umbraco.Core;
+using Umbraco.Core.Configuration.UmbracoSettings;
 using Umbraco.Core.Models;
 using Umbraco.Core.Persistence;
 using Umbraco.Tests.TestHelpers;
@@ -14,45 +22,66 @@ using Umbraco.Web;
 using Umbraco.Web.Models;
 using Umbraco.Web.Routing;
 using File = System.IO.File;
+using WebViewPageExtensions = RazorGenerator.Testing.WebViewPageExtensions;
 
 namespace Sample.Tests
 {
-    [TestFixture]
+    [Binding]
     //[DatabaseTestBehavior(DatabaseBehavior)]
     public class MinimalReadContentTypeWithGrid : BaseDatabaseFactoryTest
     {
         private UmbracoContext umbracoContext;
+        private RoutingContext routingContext;
+        private IUmbracoSettingsSection settings;
+        private string output;
 
-        [SetUp]
+        [BeforeScenario()]
         public override void Initialize()
         {
+            InitializeFixture();
+
             base.Initialize();
 
-            var settings = SettingsForTests.GenerateMockSettings();
-            SettingsForTests.ConfigureSettings(settings);
-            umbracoContext = GetUmbracoContext("http://localhost", -1, new RouteData(), true);
+            SetupUmbraco();
         }
 
-        [Test]
-        public void GetHome()
+        [When("I render (.*)")]
+        public void RenderHome(string route)
         {
-            var content = umbracoContext.ContentCache.GetByRoute("/learn/basics", true);
-
-            Assert.That(content, Is.Not.Null.And.Property("Properties").With.Count.GreaterThan(0));
-        }
-
-        [Test]
-        public void RenderHome()
-        {
-            var content = umbracoContext.ContentCache.GetByRoute("/learn/basics", true);
-            // et yadi yada
-            //umbracoContext.PublishedContentRequest = new PublishedContentRequest(new Uri("http://localhost"),  );
-            // TODO: Back to routing test
+            var content = umbracoContext.ContentCache.GetByRoute(route, true);
 
             var view = new _Views_TextPage_cshtml();
-            var output = view.Render(umbracoContext.HttpContext, new RenderModel<IPublishedContent>(content, CultureInfo.InvariantCulture));
+            var renderModel = new RenderModel<IPublishedContent>(content, CultureInfo.InvariantCulture);
+
+            StubViewContext(view, renderModel);
+
+            output = view.Render(umbracoContext.HttpContext, renderModel);
 
             Console.WriteLine(output);
+        }
+
+        [Then("the result should contain \"(.*)\"")]
+        public void TheResultShouldContain(string value)
+        {
+            Assert.That(output, Is.Not.Null.And.ContainsSubstring(value));
+        }
+
+        private void SetupUmbraco()
+        {
+            settings = SettingsForTests.GenerateMockSettings();
+            SettingsForTests.ConfigureSettings(settings);
+
+            routingContext = GetRoutingContext("http://localhost", -1, new RouteData());
+            umbracoContext = routingContext.UmbracoContext;
+            umbracoContext.PublishedContentRequest = new PublishedContentRequest(new Uri("http://localhost"), routingContext,
+                settings.WebRouting, (s) => new[] {"Kunde"});
+        }
+
+        private void StubViewContext(WebViewPage viewPage, RenderModel<IPublishedContent> renderModel)
+        {
+            var controller = Mock.Of<Controller>();
+            var controllerContext = new ControllerContext(umbracoContext.HttpContext, new RouteData(), controller);
+            viewPage.ViewContext = new ViewContext(controllerContext, Mock.Of<IView>(), new ViewDataDictionary {Model = renderModel}, new TempDataDictionary(), new StringWriter());
         }
 
         protected override string GetDbConnectionString()
@@ -60,19 +89,33 @@ namespace Sample.Tests
             return @"Data Source=..\..\..\Sample.Web\App_Data\Umbraco.sdf;Flush Interval=1;";
         }
 
-        //protected override ApplicationContext CreateApplicationContext()
-        //{
-        //    var databaseFactory = Mock.Of<IDatabaseFactory>();
-        //    var services = MockHelper.GetMockedServiceContext();
-        //    var ctx = new ApplicationContext(
-        //        //assign the db context
-        //        new DatabaseContext(databaseFactory, Logger, SqlSyntax, GetDbProviderName()),
-        //        //assign the service context
-        //        services,
-        //        CacheHelper,
-        //        ProfilingLogger);
-        //    return ctx;
-        //}
+        protected RoutingContext GetRoutingContext(string url, int templateId, RouteData routeData = null, bool setUmbracoContextCurrent = false)
+        {
+            umbracoContext = GetUmbracoContext(url, templateId, routeData);
+            var urlProvider = new UrlProvider(umbracoContext, settings.WebRouting, new IUrlProvider[]
+            {
+                new DefaultUrlProvider(settings.RequestHandler)
+            });
+
+            var rcctor = typeof (RoutingContext).GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, new[]
+            {
+                typeof (UmbracoContext),
+                typeof (IEnumerable<IContentFinder>),
+                typeof (IContentFinder),
+                typeof (UrlProvider)
+            }, new ParameterModifier[0]);
+
+            routingContext = (RoutingContext)rcctor.Invoke(new object[] {
+                umbracoContext,
+                Enumerable.Empty<IContentFinder>(),
+                Mock.Of<IContentFinder>(),
+                urlProvider});
+
+            typeof(UmbracoContext).GetProperty("RoutingContext").SetValue(umbracoContext, routingContext);
+            typeof(UmbracoContext).GetProperty("Current").SetValue(null, umbracoContext);
+
+            return routingContext;
+        }
 
         protected override string GetXmlContent(int templateId)
         {
---
+++ Sample.Tests/Rendering TextPage.feature
@@ -0,0 +1,11 @@
+﻿Feature: Rendering TextPage
+	In order to render an Umbraco textpage
+	As a developer
+	I want to be able to call .Render and get rendered HTML
+
+@mytag
+Scenario: Render learn/basics
+	When I render /learn/basics
+	Then the result should contain "Learn the basics"
+    
+
